b28c8898668a407edbe1162ee0f483be4eae3bfc
Rob Copping <robbie.copping@sonin.agency>
2023-05-04 15:10:08 +0100

    Added project to git

---
+++ CurlDeleteAnimation/Domain/Image+Model.swift
@@ -0,0 +1,13 @@
+//
+//  Image+Model.swift
+//  CurlDeleteAnimation
+//
+//  Created by Rob Copping on 02/05/2023.
+//
+
+import Foundation
+
+struct ImageModel: Identifiable {
+    var id: UUID = UUID()
+    var assetName: String
+}
